651aaf80fe0accccb0d9b4931ed839834002493f
durchhalten <46190058+durchhalten@users.noreply.github.com>
2019-10-27 22:08:36 +0800

    Update and rename README.java to firstProject.java

--- firstProject.java
+++ firstProject.java
@@ -6,7 +6,7 @@ public class firstProject{
 	
 	public static void main(String args[]){
 		
-		String path = "D:\\";
+		String path = "D:\\fcx个人资料";
 		
 		File file = new File(path);
 		
@@ -21,7 +21,11 @@ public class firstProject{
 		for(File f:fs) {
 			
 			if(f.isDirectory()) {func(f);}
-			if(f.isFile()) {System.out.println(f);}
+			if(f.isFile()) {
+				if(f.toString().endsWith(".docx")) {
+					System.out.println(f);
+				}
+			}
 			
 		}
 	
